29f4db415173dbc3da83b32ecfa50b606d371808
Chris Maltais <chris.maltais@hotmail.ca>
2018-04-13 19:53:47 -0400

    Prepare for Heroku deployment, finalize log middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 let app = express();
+const port = process.env.PORT || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+//app.use((req, res, next) => {
+//    res.render('maintenance.hbs');
+//})
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -30,6 +48,6 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is up on Port 3000')
+app.listen(port, () => {
+    console.log(`Server is up on Port ${port}`);
 });
