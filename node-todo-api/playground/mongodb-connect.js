1d8154186254016b362bfc4a69099629893e0d14
Chris Maltais <chris.maltais@hotmail.ca>
2018-05-15 21:17:33 -0400

    Initialize MongoDB

---
+++ node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,9 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    db.close();
+});
